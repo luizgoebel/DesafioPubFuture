72cde5436262a27d611798ce124174a731c4dd31
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2022-01-11 16:33:04 -0300

    dashboard

--- PubFuture/Pages/Index.cshtml.cs
+++ PubFuture/Pages/Index.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
+using PubFuture.Data;
+using PubFuture.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +13,45 @@ namespace PubFuture.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly WebContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(WebContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        [BindProperty]
+        public Receita Receita { get; set; }
 
+        [BindProperty]
+        public Conta Contas { get; set; }
+
+        [BindProperty]
+        public Despesa Despesa { get; set; }
+
+        public double TotalReceita { get; set; }
+        public double TotalDespesa { get; set; }
+        public double TotalConta { get; set; }
+        public double AcumuladoTotal { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            //ViewData["ContaID"] = new SelectList(_context.Contas, "ID", "TipoConta");
+
+            double totalRecei = _context.Receitas.Sum(c => c.Valor);
+            TotalReceita = totalRecei;
+
+            double totalDesp = _context.Despesas.Sum(c => c.Valor);
+            TotalDespesa = totalDesp;
+
+            double totalCont = _context.Contas.Sum(c => c.Saldo);
+            TotalConta = totalCont;
+
+            var acumuladoTotal = (totalRecei + totalDesp) + totalCont;
+            AcumuladoTotal = acumuladoTotal;
+
+            //await CarregarPropriedades();
+            return Page();
         }
     }
 }
